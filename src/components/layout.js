f22fd35badbbc6c7e8665065bdde4f9eba5da39c
Władek <brak@emaila.com>
2020-08-23 15:57:40 +0200

    come

--- src/components/layout.js
+++ src/components/layout.js
@@ -39,9 +39,6 @@ const Layout = props => {
               <li className="nav-about" role="menuitem">
                 <Link to={`/about`}>About</Link>
               </li>
-              <li className="nav-elements" role="menuitem">
-                <Link to={`/elements`}>Elements</Link>
-              </li>
               <li className="nav-tags" role="menuitem">
                 <Link to={`/tags`}>Tags</Link>
               </li>
