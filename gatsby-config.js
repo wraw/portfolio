f7177db3339568157cac535ae68571ae73384b8d
Władek <brak@emaila.com>
2020-08-26 22:41:08 +0200

    Wersja z poprawionym video

--- gatsby-config.js
+++ gatsby-config.js
@@ -98,6 +98,6 @@ module.exports = {
     `gatsby-plugin-netlify`,
     `gatsby-plugin-offline`,
     `gatsby-plugin-react-helmet`,
-    `gatsby-plugin-netlify-cms`,
   ],
 }
+plugins: [`gatsby-plugin-netlify-cms`]
